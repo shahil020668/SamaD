c4e5d7bf932edfa542aedda5bbef8a5e9760e17e
zynox <mohammadshahil0068@gmail.com>
2025-12-26 10:19:46 +0530

    msg

--- SamaD.py
+++ SamaD.py
@@ -1,5 +1,5 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
-from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 from dotenv import load_dotenv
 import streamlit as st
 
@@ -7,16 +7,16 @@ import streamlit as st
 load_dotenv()
 
 llm = HuggingFaceEndpoint(
-    repo_id='meta-llama/Llama-3.1-8B-Instruct',
+    repo_id='deepseek-ai/DeepSeek-V3.2',
     task = "text-generation"
 
 )
 model = ChatHuggingFace(llm=llm)
 
-st.title("💬 Chatbot")
+st.title("💬 SamaD")
 
 if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [] 
+    st.session_state.chat_history = [SystemMessage(content="You are a helpful assistant. You must always respond in English.")]
 
 
 user_input = st.chat_input("Ask anything")
@@ -29,10 +29,13 @@ if user_input:
 
     st.session_state.chat_history.append(AIMessage(content=result.content))
 
+count = 0
 
 for msg in st.session_state.chat_history:
-    if isinstance(msg, HumanMessage):
-        st.chat_message("user").write(msg.content)
-    else:
-        st.chat_message("assistant").write(msg.content)
+    if count != 0:
+        if isinstance(msg, HumanMessage):
+            st.chat_message("user").write(msg.content)
+        else:
+            st.chat_message("assistant").write(msg.content)
+    count = count + 1
         
